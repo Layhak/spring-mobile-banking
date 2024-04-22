d0837fe1191b242ff8641c18c97087d9c99b22e2
LAYHAK <layhak.garu@gmail.com>
2024-04-22 16:18:43 +0700

    update table Authority and add Authority Repository

--- src/main/java/co/istad/mobilebankingcstad/domain/Authority.java
+++ src/main/java/co/istad/mobilebankingcstad/domain/Authority.java
@@ -1,8 +1,14 @@
 package co.istad.mobilebankingcstad.domain;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 @Table(name = "authorities_tbl")
 public class Authority {
     @Id
