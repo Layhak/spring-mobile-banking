48cc0dc0067edccd50acd10697538db6d1c486a8
LAYHAK <layhak.garu@gmail.com>
2024-04-22 16:22:59 +0700

    Add authorityInit and update roleInit

--- src/main/java/co/istad/mobilebankingcstad/init/DataInitializer.java
+++ src/main/java/co/istad/mobilebankingcstad/init/DataInitializer.java
@@ -1,8 +1,10 @@
 package co.istad.mobilebankingcstad.init;
 
 import co.istad.mobilebankingcstad.domain.AccountType;
+import co.istad.mobilebankingcstad.domain.Authority;
 import co.istad.mobilebankingcstad.domain.Role;
 import co.istad.mobilebankingcstad.features.accounttype.AccountTypeRepository;
+import co.istad.mobilebankingcstad.features.authority.AuthorityRepository;
 import co.istad.mobilebankingcstad.features.roles.RoleRepository;
 import jakarta.annotation.PostConstruct;
 import jakarta.persistence.PrePersist;
@@ -11,7 +13,9 @@ import org.hibernate.dialect.function.array.ArraySliceUnnestFunction;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 // populate database ( role with some data )
@@ -20,12 +24,35 @@ import java.util.List;
 public class DataInitializer {
     private final RoleRepository roleRepository;
     private final AccountTypeRepository accountTypeRepository;
+    private final AuthorityRepository authorityRepository;
+
     @PostConstruct
-    void roleInit(){
-        List<String> roles = List.of("ADMIN","STUFF","USER");
-        if(roleRepository.findAll().isEmpty()){
-            for(var role : roles){
+    void authorityInit() {
+        List<String> authorities = List.of("Read", "Write", "Delete");
+        if (authorityRepository.findAll().isEmpty()) {
+            authorities.forEach(auth -> {
+                Authority authority = new Authority();
+                authority.setName(auth);
+                authorityRepository.save(authority);
+            });
+        }
+    }
+
+    @PostConstruct
+    void roleInit() {
+        List<String> roles = List.of("Admin", "User");
+        if (roleRepository.findAll().isEmpty()) {
+            var allAuth = new HashSet<>(authorityRepository.findAll());
+            for (var role : roles) {
                 var roleObj = new Role();
+                if (role.equals("Admin")) {
+                    roleObj.setAuthorities(new HashSet<>(allAuth));
+                } else if (role.equals("User")) {
+                    roleObj.setAuthorities(allAuth.stream().filter(authority -> authority
+                            .getName()
+                            .equals("Read"))
+                            .collect(Collectors.toSet()));
+                }
                 roleObj.setName(role);
                 roleRepository.save(roleObj);
             }
@@ -34,8 +61,8 @@ public class DataInitializer {
     }
 
     @PostConstruct
-    void accountTypesInit(){
-        List<AccountType> accountTypes = new ArrayList<>(){{
+    void accountTypesInit() {
+        List<AccountType> accountTypes = new ArrayList<>() {{
             add(new AccountType()
                     .setName("SAVINGS")
                     .setDescription("This is the type of account that you gain interest when you save your money in the bank"));
@@ -44,14 +71,15 @@ public class DataInitializer {
                     .setName("PAYROLLS")
                     .setDescription("This is account to get paid by company monthly."));
             add(new AccountType()
-                    .setName("Card")
+                    .setName("CARD")
                     .setDescription("Allow you to create different card for personal uses!"));
         }};
 
-        if(accountTypeRepository.findAll().isEmpty())
-        {
+        if (accountTypeRepository.findAll().isEmpty()) {
             accountTypeRepository.saveAll(accountTypes);
 
         }
     }
+
+
 }
