bc075a6acb04cf5e3ec1d7650cac5e784ac155cb
LAYHAK <layhak.garu@gmail.com>
2024-04-23 16:20:17 +0700

    add new config for public and private key AccessToken and Refresh Token that gererate in KeyUtils

--- src/main/java/co/istad/mobilebankingcstad/config/SecurityConfiguration.java
+++ src/main/java/co/istad/mobilebankingcstad/config/SecurityConfiguration.java
@@ -2,9 +2,18 @@ package co.istad.mobilebankingcstad.config;
 
 import co.istad.mobilebankingcstad.security.CustomUserDetailService;
 import co.istad.mobilebankingcstad.security.JwtToUserConverter;
+import co.istad.mobilebankingcstad.security.KeyUtils;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +21,11 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
 import org.springframework.security.oauth2.server.resource.web.BearerTokenAuthenticationEntryPoint;
 import org.springframework.security.oauth2.server.resource.web.access.BearerTokenAccessDeniedHandler;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,6 +35,9 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
     private final CustomUserDetailService userDetailService;
     private final JwtToUserConverter jwtToUserConverter;
+    private final KeyUtils keyUtils;
+
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -51,13 +68,62 @@ public class SecurityConfiguration {
                 .formLogin(AbstractHttpConfigurer::disable)//*disable default login form
                 .httpBasic(Customizer.withDefaults())//*basic auth
                 .oauth2ResourceServer(
-                        (oauth2)->oauth2.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtToUserConverter))
+                        (oauth2) -> oauth2.jwt(jwtConfigurer -> jwtConfigurer.jwtAuthenticationConverter(jwtToUserConverter))
                 )
                 .sessionManagement(
-                        (session)->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                        (session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
-                .exceptionHandling(ex->ex.authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
-                    .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
+                        .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
                 ).build();
     }
+
+    //todo related to jwtEncoder and Decoder
+    @Bean
+    @Qualifier("jwtRefreshTokenEncoder")
+    JwtEncoder jwtRefreshTokenEncoder() {
+        JWK jwk = new RSAKey.Builder(keyUtils.getRefreshTokenPublicKey())
+                .privateKey(keyUtils.getRefreshTokenPrivateKey())
+                .build();
+        JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwkSource);
+    }
+
+    @Bean
+    @Qualifier("jwtRefreshTokenDecoder")
+    JwtDecoder jwtRefreshTokenDecoder() {
+        return NimbusJwtDecoder
+                .withPublicKey(keyUtils.getRefreshTokenPublicKey())
+                .build();
+    }
+
+    @Bean
+    @Primary
+    JwtEncoder jwtAccessTokenEncoder() {
+        JWK jwk = new RSAKey.Builder(keyUtils.getAccessTokenPublicKey())
+                .privateKey(keyUtils.getAccessTokenPrivateKey())
+                .build();
+        JWKSource<SecurityContext> jwkSource = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwkSource);
+    }
+
+    @Bean
+    @Primary
+    JwtDecoder jwtAccessTokenDecoder() {
+        return NimbusJwtDecoder
+                .withPublicKey(keyUtils.getAccessTokenPublicKey())
+                .build();
+    }
+
+    @Bean
+    @Qualifier("refreshTokenAuthProvider")
+    JwtAuthenticationProvider refreshTokenAuthProvider() {
+        JwtAuthenticationProvider provider = new JwtAuthenticationProvider(
+                jwtRefreshTokenDecoder()
+        );
+        provider.setJwtAuthenticationConverter(jwtToUserConverter);
+        return provider;
+
+    }
+
 }
