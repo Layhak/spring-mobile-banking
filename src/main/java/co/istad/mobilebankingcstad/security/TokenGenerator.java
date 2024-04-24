2dc840c0e6b726e8029bcdf4121ea4e4732d7017
LAYHAK <layhak.garu@gmail.com>
2024-04-24 10:10:09 +0700

    create generate token for authresponse

--- src/main/java/co/istad/mobilebankingcstad/security/TokenGenerator.java
+++ src/main/java/co/istad/mobilebankingcstad/security/TokenGenerator.java
@@ -2,12 +2,15 @@ package co.istad.mobilebankingcstad.security;
 
 import co.istad.mobilebankingcstad.features.auth.dto.AuthResponse;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
@@ -52,11 +55,29 @@ public class TokenGenerator {
         return jwtRefreshTokenEncoder.encode(JwtEncoderParameters.from(claimsSet)).getTokenValue();
     }
 
-    public AuthResponse generateToken() {
+    public AuthResponse generateToken(Authentication authentication) {
+
+        if (!(authentication.getPrincipal() instanceof CustomUserDetail customUserDetail)) {
+            throw new BadCredentialsException("Provided Token is not valid");
+        }
+        String refreshToken;
+        if (authentication.getCredentials() instanceof Jwt jwt) {
+            Instant now = Instant.now(),
+                    expireAt = jwt.getExpiresAt();
+            Duration duration = Duration.between(now, expireAt);
+            long daysUntilExpire = duration.toDays();
+            if (daysUntilExpire < 7) {
+                refreshToken = createRefreshToken(authentication);
+            } else {
+                refreshToken = jwt.getTokenValue();
+            }
+        } else {
+            refreshToken = createRefreshToken(authentication);
+        }
         return AuthResponse.builder()
-                .userId("1234567")
-//                .accessToken(createAccessToken())
-//                .refreshToken(createRefreshToken())
+                .userId(customUserDetail.getUser().getId())
+                .accessToken(createAccessToken(authentication))
+                .refreshToken(refreshToken)
                 .build();
     }
 }
